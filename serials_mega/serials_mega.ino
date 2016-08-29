e007b280ce3b6976d61bdce89924160e50b5ef18
Samuel Sau Kin Chan <chansaukin@gmail.com>
2016-08-29 18:46:28 +0800

    ask mega to send a string, nano to echo, and mega to catch it
    IMPORTANT: pin 18 of MEGA is TX1, pin 19 of MEGA is RX1.
    Some mega boards will get that wrong.

--- serials_mega/serials_mega.ino
+++ serials_mega/serials_mega.ino
@@ -13,25 +13,33 @@
 
 #include <SoftwareSerial.h>
 
-SoftwareSerial serialNano0(10, 22); // RX, TX
-
+//we can turn it to an array later
+SoftwareSerial serialNano0(10, 12); // RX, TX
 
 void setup() {
-  Serial.begin(115200);  //USB
-  Serial1.begin(115200); //broadcast
-  serialNano0.begin(115200);
-  Serial.println("Mega is online.");
-
+  Serial1.begin(9600);  //USB
+  Serial.begin(9600); //broadcast
+  //serialNano0.begin(9600);
+  //Serial.println("Mega is online.");Serial.flush();
 }
 
 void loop() {
-  serialNano0.listen();
-  if (serialNano0.available() > 0){
-    String feedback = serialNano0.readStringUntil('\n');
-    Serial.println("received from serialNano0: " + feedback);
-  } else {
-    Serial.println("No response.");
-  }
+  //request feedback from nano0
+  String request = "f0";
+  Serial1.println(request);
+  Serial.println("sent: " + request);Serial.flush();
+
+  //serialNano0.listen();
 
   delay(1000);
+
+  //read feedback
+  if (Serial1.available() > 0){
+    String feedback = Serial1.readStringUntil('\n');
+    Serial.println("received from Serial1: " + feedback);Serial.flush();
+  } else {
+    //Serial.println("no response");Serial.flush();
+  }
+
+  delay(2000);
 }
--- serials_nano/serials_nano.ino
+++ serials_nano/serials_nano.ino
@@ -3,15 +3,34 @@
  * 
  * receive via TX0
  * send via RX0
+ * 
+ * software serial baud rate: max. 57600 for 8MHz CPU, 
+ * 115200 for 16MHz CPU (mega, nano)
  */
 
 #define THIS_NANO_ID 0
+int led = 13;
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
+  pinMode(led, OUTPUT);
+  digitalWrite(led,LOW);
 }
 
 void loop() {
-  Serial.println("Hi, I'm nano 0.");
+  //wait and read request
+  if (Serial.available() > 0){
+    String requestReceived = Serial.readStringUntil('\n');
+    Serial.println(requestReceived);  //echo the string received
+    Serial.flush();
+    digitalWrite(led,HIGH);
+//    if (requestReceived == ("f" + THIS_NANO_ID)){  //if it's a feedback request for this nano
+//      Serial.println(THIS_NANO_ID + "777");                     //send feedback
+//    }
+
+  } else {
+    //Serial.println("I can't here you!");
+    //Serial.flush();
+  }
   delay(3000);
 }
