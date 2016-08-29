e89fd25fd9ec69acf4ed3b47fa786fc1854928c7
Samuel Sau Kin Chan <chansaukin@gmail.com>
2016-08-29 15:12:08 +0800

    test one-way serial transmission from nano RX0 to mega softwareSerial

--- serials_mega/serials_mega.ino
+++ serials_mega/serials_mega.ino
@@ -3,53 +3,35 @@
  * 
  * send via TX1
  * receive via digital pins
+ * 
+ * Not all pins on the Mega and Mega 2560 support change interrupts, 
+ * so only the following can be used for RX: 
+ * 10, 11, 12, 13, 14, 15, 50, 51, 52, 53, 
+ * A8 (62), A9 (63), A10 (64), A11 (65), 
+ * A12 (66), A13 (67), A14 (68), A15 (69).
  */
 
 #include <SoftwareSerial.h>
 
-SoftwareSerial serialNano0(22, 23); // RX, TX
-SoftwareSerial serialNano1(24, 25); // RX, TX
+SoftwareSerial serialNano0(10, 22); // RX, TX
+
 
 void setup() {
   Serial.begin(115200);  //USB
   Serial1.begin(115200); //broadcast
   serialNano0.begin(115200);
-  serialNano1.begin(115200);
   Serial.println("Mega is online.");
 
 }
 
 void loop() {
-  //request feedback from nano0
-  String request = "f0";
-  Serial1.println(request);
-  Serial.println("sent: " + request);
-
   serialNano0.listen();
-  //wait
-  delay(1000);
-
-  //read feedback
   if (serialNano0.available() > 0){
     String feedback = serialNano0.readStringUntil('\n');
     Serial.println("received from serialNano0: " + feedback);
+  } else {
+    Serial.println("No response.");
   }
 
-  //request feedback from nano1
-  request = "f1";
-  Serial1.println(request);
-  Serial.println("sent: " + request);
-
-  serialNano1.listen();
-  //wait
-  delay(50);
-
-  //read feedback
-  if (serialNano1.available() > 0){
-    String feedback = serialNano1.readStringUntil('\n');
-    Serial.println("received from serialNano1: " + feedback);
-  }
-
-  delay(3000);
-
+  delay(1000);
 }
--- serials_nano/serials_nano.ino
+++ serials_nano/serials_nano.ino
@@ -12,13 +12,6 @@ void setup() {
 }
 
 void loop() {
-  //wait and read request
-  if (Serial.available() > 0){
-    String requestReceived = Serial.readStringUntil('\n');
-    if (requestReceived == ("f" + THIS_NANO_ID)){  //if it's a feedback request for this nano
-      Serial.println("Im here."); 
-      //Serial.println(THIS_NANO_ID + "777");                     //send feedback
-    }
-  }
-
+  Serial.println("Hi, I'm nano 0.");
+  delay(3000);
 }
